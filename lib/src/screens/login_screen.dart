1bcc6f60f8cedc7b40e0b3e28f7e6c0065491aaa
Barba <hated.kuroitsuki@gmail.com>
2025-07-06 09:10:12 -0300

    REAL ADVANCED VALIDATION

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -8,16 +8,20 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(context) {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Form(
+        key: formKey,
         child: Column(
           children: [
             emailField(),
             passwordField(), 
-            submitButton()
+            Container(margin: EdgeInsets.only(top: 25.0)),
+            submitButton(),
           ],
         ),
       ),
@@ -31,23 +35,49 @@ class LoginScreenState extends State<LoginScreen> {
         labelText: "Email Adress",
         hintText: "youremail@gmail.com",
       ),
+      validator: (String? value) {
+        //return null if the input is valid
+        //return a string if the input is invalid
+
+        //this wasnt necessary until dart 2.12, where null safety was introduced
+        if (value == null || value.isEmpty) {
+          return "Please enter your email address.";
+        }
+        if (!value.contains("@") || value.isEmpty) {
+          return "Please enter a valid email address.";
+        }
+        return null; //input is valid
+      },
     );
+    
   }
 
   Widget passwordField() {
+    // margin: EdgeInsets.only(bottom: 20.0),
     return TextFormField(
       obscureText: true,
       decoration: InputDecoration(
         labelText: "Password",
         hintText: "Your password",
       ),
-    );
-  }
+      validator: (value){
+        if (value == null || value.isEmpty) {
+          return "Please enter your password.";
+        }
+        if (value.length < 6) {
+          return "Password must be at least 6 characters long.";
+        }
+        return null; //input is valid
+      },
+    );  
+}
+  
 
 Widget submitButton() {
   return ElevatedButton(
     onPressed: () {
-      // ação ao pressionar
+      //reset -> clear the form
+      print(formKey.currentState?.validate());
     },
     style: ElevatedButton.styleFrom(
       backgroundColor: Colors.blue,
